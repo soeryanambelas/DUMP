d8cb01d64926881874bdbbc0cf6d98d6765006fb
soeryanambelas <123479149+soeryanambelas@users.noreply.github.com>
2023-12-03 22:28:37 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,15 @@
+import os,requests
+xr = requests.get("http://ip-api.com/json/").json()
+try:
+	fc = xr["country"]
+except KeyError:
+	print("[*] koneksi anda buruk,silahkan refresh data anda.")
+	exit()
+	
+if __name__ == "__main__":
+	os.system("git pull")
+	if "Indonesia" == fc:
+		__import__("dump").menu()
+	else:
+		print("[*] tools ini hanya support/berlaku di indonesia.")
+		exit()
